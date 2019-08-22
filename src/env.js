ded7f72fda29620c6f863c6542a24609a72bdb96
Alex Warren <exrhizo@gmail.com>
2019-08-22 01:01:51 -0700

    revert env to use regex to check local

--- src/env.js
+++ src/env.js
@@ -1,7 +1,6 @@
 const ENV_REGEX = /http(?:s)?:\/\/(local\.|dev\.|staging\.)?app\.thehumanfactor\.ai/;
 
 export const getEnv = () => {
-  return "local";
   const match = ENV_REGEX.exec(window.location.href);
   if (match) {
     // eslint-disable-next-line
