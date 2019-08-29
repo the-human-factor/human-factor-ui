db655b90f40d350c7b9692c2a15d4a70915a795f
Leo Urbina <leo.a.urbina@gmail.com>
2019-08-28 18:17:29 -0700

    Fixes env detection, adds tests

--- src/config.js
+++ src/config.js
@@ -1,13 +1,6 @@
 import { getEnv } from "./env.js";
 
 let env = getEnv();
-const overrideEnv = window.localStorage.getItem("env");
-
-if (["dev", "prod", "local"].includes(overrideEnv)) {
-  console.log(`Overriding env from localStorage [${overrideEnv}]`);
-  env = overrideEnv;
-}
-
 const CONFIG = {
   local: {
     API: "http://localhost:3000/api"
---
+++ src/env.test.js
@@ -0,0 +1,22 @@
+import { detectEnv } from "./env";
+
+describe.each([
+  ["https://localhost:9000", "local"],
+  ["http://localhost:9000", "local"],
+
+  ["https://local.app.thehumanfactor.ai", "local"],
+  ["http://local.app.thehumanfactor.ai", "local"],
+
+  ["https://staging.app.thehumanfactor.ai", "staging"],
+  ["http://staging.app.thehumanfactor.ai", "staging"],
+
+  ["https://app.thehumanfactor.ai", "prod"],
+  ["http://app.thehumanfactor.ai", "prod"],
+
+  ["some-randomother-thing", "staging"]
+])("Environment detection of %s", (url, expected) => {
+  test(`should return ${expected}`, () => {
+    console.log("URL", url, "expected", expected);
+    expect(detectEnv(url)).toBe(expected);
+  });
+});
