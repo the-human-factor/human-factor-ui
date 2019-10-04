6a7c8626d66afb1216da148858e4d64e2bbfa3be
Alex Warren <exrhizo@gmail.com>
2019-10-03 23:51:13 -0700

    Add favicons and generate index.html

---
+++ manifest.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  appName: 'The Human Factor',
+  appDescription: 'Emotional Intelligence Made Actionable.',
+  start_url: '.',
+  theme_color: '#000',
+  background_color: '#fff',
+  logo: path.resolve(__dirname, 'src/images/icon.png')
+};
